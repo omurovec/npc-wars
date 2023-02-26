7c6b11bc341e6fe84d07f64f88c8dccfff92affe
omurovec <owenmurovec@gmail.com>
2023-02-26 16:30:11 -0700

    fix: move ezkl to api

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,18 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use ezkl::pfsys::{gen_srs};
+use halo2_proofs::poly::kzg::commitment::KZGCommitmentScheme;
+use halo2curves::bn256::{Bn256};
+
+const K: u32 = 17;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
+#[post("/getVerifierBytecode")]
 async fn echo(req_body: String) -> impl Responder {
+    let params = gen_srs::<KZGCommitmentScheme<Bn256>>(K);
     HttpResponse::Ok().body(req_body)
 }
 
